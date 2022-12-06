a21c37561aa6f10ca22637dde0bd9fa94bc256b3
Minna1245 <minna.viitanen@outlook.com>
2022-12-06 12:07:01 +0100

    added function for deleting contacts

--- assess1.py
+++ assess1.py
@@ -18,6 +18,12 @@ def add_contact(conn, first_name, last_name, title, organization):
     cur.execute(f"INSERT INTO contacts (first_name, last_name, title, organization) VALUES ('{first_name}', '{last_name}', '{title}', '{organization}');")
     cur.close()
 
+def delete_contact(conn, first_name):
+    cur = conn.cursor()
+    cur.execute(f"DELETE FROM contacts WHERE first_name = '{first_name}';")
+    cur.close()
+
+
 
 while True: 
     cmd = input("Command: ").strip().lower()
@@ -32,7 +38,7 @@ while True:
         add_contact(conn, first_name, last_name, title, organization)
         print(f"{first_name} added!")
     elif cmd == "delete":
-        first_name = input("  First_name: ")
+        first_name = input("  First_name: ").strip().capitalize()
         delete_contact(conn, first_name)
         print(f"{first_name} deleted!")
     else:
