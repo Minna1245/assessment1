01152fe1e8da134d8933bef0213da9e9cdaed4d0
Minna1245 <minna.viitanen@outlook.com>
2022-12-06 11:15:36 +0100

    added function that fetches data from database

--- assess1.py
+++ assess1.py
@@ -5,6 +5,11 @@ conn = psycopg2.connect(
     user="postgres",
     password="Sqlkone")
 
+def get_data(conn):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM view_contacts;")
+    rows = cur.fetchall()
+    cur.close()
+    return rows
 
 
-    
