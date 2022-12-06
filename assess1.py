96b44c2dd16005c8b6e23a1aeb7c64d988771ba1
Minna1245 <minna.viitanen@outlook.com>
2022-12-06 11:45:40 +0100

    added while loop to get input from user

--- assess1.py
+++ assess1.py
@@ -13,3 +13,21 @@ def get_data(conn):
     return rows
 
 
+while True: 
+    cmd = input("Command: ").strip().lower()
+    if cmd == "list":
+        for name1, name2, c, ct, cc in (get_data(conn)):
+            print(name1, "\t", name2, "\t", c, "\t", ct, "\t", cc)
+    elif cmd == "insert":
+        first_name = input("  First Name: ").strip().capitalize()
+        last_name = input("  Last name: ").strip().capitalize()
+        title = input("  Title: ").strip().capitalize()
+        organization = input("  Organization:  ").strip().capitalize()
+        add_contact(conn, first_name, last_name, title, organization)
+        print(f"{first_name} added!")
+    elif cmd == "delete":
+        first_name = input("  First_name: ")
+        delete_contact(conn, first_name)
+        print(f"{first_name} deleted!")
+    else:
+        print("Unidentified command, please repeat")    
